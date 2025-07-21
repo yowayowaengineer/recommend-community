ab4273dd3beab5af8551639e8686aff5f493b5fe
くらもとやすひろ <kuramoto@pscsrv.co.jp>
2025-07-21 15:54:52 +0900

    💄 コンテスト背景を適用

--- app/app.vue
+++ app/app.vue
@@ -1,37 +1,18 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <swiper-container>
-      <swiper-slide><Glass /></swiper-slide>
-      <swiper-slide>Slide 2</swiper-slide>
-      <swiper-slide>Slide 3</swiper-slide>
-    </swiper-container>
+    <MovieBackground>
+      <swiper-container>
+        <swiper-slide></swiper-slide>
+        <swiper-slide><Glass /></swiper-slide>
+        <swiper-slide><Glass /></swiper-slide>
+      </swiper-container>
+    </MovieBackground>
   </div>
 </template>
 
 <script setup>
 import { register } from 'swiper/element/bundle'
+import MovieBackground from './components/MovieBackground.vue'
 register()
 </script>
-
-<style lang="css">
-body {
-  background: linear-gradient(65deg, #231557, #44107a, #ff1361, #fff800);
-  background-size: 1000% 1000%;
-  animation: GradientBackground 20s ease infinite;
-}
-
-@keyframes GradientBackground {
-  0% {
-    background-position: 0% 50%;
-  }
-
-  50% {
-    background-position: 100% 50%;
-  }
-
-  100% {
-    background-position: 0% 50%;
-  }
-}
-</style>
---
+++ app/components/MovieBackground.vue
@@ -0,0 +1,9 @@
+<template>
+  <video-background
+    src="https://pub-af3b1202949a4aa5b69d7ad4fb31c227.r2.dev/pic_promotion.mp4"
+    poster="/image/pic_promotion.webp"
+    style="height: 100vh"
+  >
+    <slot />
+  </video-background>
+</template>
