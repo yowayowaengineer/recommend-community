4736d089dbadc7d2b35b1671fecf58b95d7e730d
くらもとやすひろ <kuramoto@pscsrv.co.jp>
2025-07-21 18:23:33 +0900

    ✨ 画像表示用のコンポーネントを追加しつつスライド記載

--- app/app.vue
+++ app/app.vue
@@ -6,20 +6,29 @@
         <swiper-slide></swiper-slide>
         <swiper-slide>
           <Glass title="コミュニティによって得られるもの">
-            <p>あいうえおかきくけこさしすせそ</p>
-            <p>あいうえおかきくけこさしすせそ</p>
-            <p>あいうえおかきくけこさしすせそ</p>
-            <p>あいうえおかきくけこさしすせそ</p>
+            <div
+              class="flex flex-col justify-center items-center h-[50vh] gap-4"
+            >
+              <p>イノベーション推進事業本部　デジタル推進部</p>
+              <p>コンテスト実行委員会　顧問</p>
+              <p>くらもとやすひろ</p>
+            </div>
           </Glass>
         </swiper-slide>
-        <swiper-slide><Glass title="2. たいとる" /></swiper-slide>
+        <swiper-slide>
+          <Glass title="今年のコンテストどうおもう？">
+            <ImageViewer
+              src="/image/pic_promotion.webp"
+              alt="PIC GAME AWARD 2025"
+            />
+          </Glass>
+        </swiper-slide>
       </swiper-container>
     </MovieBackground>
   </div>
 </template>
 
-<script setup>
+<script lang="ts" setup>
 import { register } from 'swiper/element/bundle'
-import MovieBackground from './components/MovieBackground.vue'
 register()
 </script>
---
+++ app/components/ImageViewer.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="image-container">
+    <img :src="src" :style="imageStyle" @click="toggleZoom" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  src: string
+  alt?: string
+}>()
+
+const isZoomed = ref(false)
+
+const toggleZoom = () => {
+  isZoomed.value = !isZoomed.value
+}
+
+const imageStyle = computed(() => {
+  return isZoomed.value
+    ? {
+        transform: 'scale(1.25)',
+        position: 'absolute',
+        left: '0px',
+        top: '0px',
+      }
+    : {}
+})
+</script>
+
+<style lang="css" scoped>
+.image-container {
+  width: auto;
+  height: 50vh;
+  overflow: hidden;
+}
+
+img {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+  transition: transform 0.3s ease;
+  cursor: pointer;
+}
+</style>
