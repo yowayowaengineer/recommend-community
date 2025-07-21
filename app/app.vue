3975ad4176d390419a798c52ffcfe23f05a4d1d3
くらもとやすひろ <kuramoto@pscsrv.co.jp>
2025-07-21 16:52:01 +0900

    🧑‍💻 スライドを書きやすいようにコンポーネントを変更

--- app/app.vue
+++ app/app.vue
@@ -4,8 +4,15 @@
     <MovieBackground>
       <swiper-container>
         <swiper-slide></swiper-slide>
-        <swiper-slide><Glass /></swiper-slide>
-        <swiper-slide><Glass /></swiper-slide>
+        <swiper-slide>
+          <Glass title="コミュニティによって得られるもの">
+            <p>あいうえおかきくけこさしすせそ</p>
+            <p>あいうえおかきくけこさしすせそ</p>
+            <p>あいうえおかきくけこさしすせそ</p>
+            <p>あいうえおかきくけこさしすせそ</p>
+          </Glass>
+        </swiper-slide>
+        <swiper-slide><Glass title="2. たいとる" /></swiper-slide>
       </swiper-container>
     </MovieBackground>
   </div>
--- app/components/Glass.vue
+++ app/components/Glass.vue
@@ -1,23 +1,25 @@
 <template>
   <div class="h-screen w-screen flex justify-center items-center">
     <div class="glass">
-      <h1>1. タイトルとかTitle...</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-        laborum inventore nulla illum, eaque sint culpa. Repellat aliquid nemo
-        vero nesciunt placeat voluptate aspernatur sapiente? Dolorum
-        necessitatibus officiis nobis laboriosam!
-        あいうえおかきくけこさしすせそたちつてと
-      </p>
+      <h1>{{ title }}</h1>
+      <div class="mt-8 flex flex-col gap-2">
+        <slot />
+      </div>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+defineProps<{
+  title?: string
+}>()
+</script>
 
 <style lang="css" scoped>
 .glass {
   width: 80%;
   height: 80vh;
-  padding: 2rem;
+  padding: 4rem 6rem;
   background-color: rgba(255, 255, 255, 0.1);
   border: 1px solid rgba(255, 255, 255, 0.4);
   border-right-color: rgba(255, 255, 255, 0.2);
@@ -37,10 +39,9 @@
   letter-spacing: 0.06em;
 }
 
-.glass p {
-  margin-top: 20px;
+.glass div {
   color: #fff;
-  text-shadow: 0 0 20px rgba(0, 0, 0, 0.6);
-  font-size: 16px;
+  text-shadow: 0 0 20px rgba(0, 0, 0, 0.7);
+  font-size: 24px;
 }
 </style>
