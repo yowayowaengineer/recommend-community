cc6e4c72e1fedc5a50b0711fe9ae6027628c585b
くらもとやすひろ <kuramoto@pscsrv.co.jp>
2025-07-21 18:31:26 +0900

    🚧 とりあえずタイトルだけ追加

--- app/app.vue
+++ app/app.vue
@@ -16,13 +16,29 @@
           </Glass>
         </swiper-slide>
         <swiper-slide>
-          <Glass title="今年のコンテストどうおもう？">
+          <Glass title="今年のコンテストどう思う？">
             <ImageViewer
               src="/image/pic_promotion.webp"
               alt="PIC GAME AWARD 2025"
             />
           </Glass>
         </swiper-slide>
+        <swiper-slide>
+          <Glass title="前回のゲームコンテストとの違い"> </Glass>
+        </swiper-slide>
+        <swiper-slide>
+          <Glass title="どうしてそのような発想が生まれたのか？"> </Glass>
+        </swiper-slide>
+        <swiper-slide>
+          <Glass title="自分の視点と他者の視点"> </Glass>
+        </swiper-slide>
+        <swiper-slide>
+          <Glass title="視野を広げることができるのが、そう……"> </Glass>
+        </swiper-slide>
+        <swiper-slide>
+          <Glass title="コミュニティ参加のすゝめ"> </Glass>
+        </swiper-slide>
+        <swiper-slide></swiper-slide>
       </swiper-container>
     </MovieBackground>
   </div>
